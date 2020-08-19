309ad9485308aefa06eb25a609d6b69409b229c3
Lynda Santiago <you@example.com>
2020-08-18 22:24:19 -0400

    set up post request and its working successfully

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,14 +1,25 @@
-import React from 'react';
-import './App.css';
-import Login from './components/Login'
+import React from "react";
+import "./App.css";
+import Login from "./components/Login";
+import { Route } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <header><img src="https://images.spoilertv.com/cache/archived-old-shows/friends/season_10/friends-logo-white-type_FULL.jpg"/></header>
-    
-     <Login/>
+      <header>
+        <img
+          src="https://images.spoilertv.com/cache/archived-old-shows/friends/season_10/friends-logo-white-type_FULL.jpg"
+          alt=""
+        />
+      </header>
+      <Route exact path="/">
+        <Login />
+      </Route>
     </div>
   );
 }
 
 export default App;
+
+//The login function should save the returned token to localStorage.
+//You can setup isLoading state in your Login component, and show a spinner
+//on your form or in your button while the login request is happening.
--- friends/src/components/Login.jsx
+++ friends/src/components/Login.jsx
@@ -1,17 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 export default function Login() {
+  const [credentials, setCredentials] = useState({
+    username: "Lambda School",
+    password: "i<3Lambd4",
+  });
+  const inputHandler = (e) => {
+    setCredentials({ [e.target.name]: e.target.value });
+  };
+  const login = (e) => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:5000/api/login", credentials)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((error) => {
+        console.log("error happend with the post request to API", error);
+      });
+  };
   return (
-    <div className="form"> 
-      <form>
-        <label htmlFor="username"><p>Username</p><br/>
-          <input id="username" name="username" />
-        </label><br/>
-        <label htmlFor="password"><p>Password</p><br/>
-          <input id="password" name="password" />
-        </label><br/>
+    <div className="form">
+      <form onSubmit={login}>
+        <label htmlFor="username">
+          <p>Username</p>
+          <br />
+          <input
+            type="text"
+            id="username"
+            name="username"
+            placeholder="username . . ."
+            value={credentials.username}
+            onChange={inputHandler}
+          />
+        </label>
+        <br />
+        <label htmlFor="password">
+          <p>Password</p>
+          <br />
+          <input
+            type="password"
+            id="password"
+            name="password"
+            placeholder="password . . ."
+            value={credentials.password}
+            onChange={inputHandler}
+          />
+        </label>
+        <br />
         <button type="submit">Login</button>
-        
       </form>
     </div>
   );
